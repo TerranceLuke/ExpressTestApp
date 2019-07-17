73edd7da53eb79fd02a5b0b1828f077359494ef6
MastaT1 <turon48203@yahoo.com>
2019-07-17 17:52:57 -0400

    initial app

--- ExpressTestApp/routes/index.js
+++ ExpressTestApp/routes/index.js
@@ -2,9 +2,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
+var getData = function () {
+    var data = {
+        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
+    }
+    return data;
+}
 
-module.exports = router;
+
+
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express', "data": getData() });
+});
